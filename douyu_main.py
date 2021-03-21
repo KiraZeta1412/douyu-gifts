688897ec79fceeafa97309aff01abba91ff58d39
KiraZeta1412 <72505513+KiraZeta1412@users.noreply.github.com>
2021-03-21 15:19:06 +0800

    Update douyu_main.py

--- douyu_main.py
+++ douyu_main.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import json
 import time
+import os
 def browser_initial():
     # chrome_options = Options()
     # chrome_options.add_argument('--headless')
@@ -15,8 +16,7 @@ def browser_initial():
 
 def log_csdn(browser):
     #输入你的cookies
-    mycookies = ('[{"name": "PHPSESSID", "value": "ma95k7oj5t0f5fg7luptql5kv0", "path": "/", "domain": "www.douyu.com", "secure": false, "httpOnly": true, "sameSite": "None"}, {"name": "acf_auth", "value": "0a21fY4EvTrxdmHGFwiWjeZdS9LACFp4RB2LNrzQqPahZQuVpsbICUZLrwv70xWVsv5pMospFXXhrgsFWF%2F159laK%2BlNU7ivoSPVqgzJnBUDbeJQKwT96%2B4", "path": "/", "domain": "www.douyu.com", "secure": false, "httpOnly": true, "expiry": 1616158558, "sameSite": "None"}, {"name": "dy_auth", "value": "9300HtDfM2NaOWY%2BUx%2BrjDZlfJYN7m0IiBRhiCMWfnajFmLJHqQiA8GXZzKXagILUlp3ctDmikMayxyuaQMUR%2FlXhOOGskoayJcwWIuSiMMaarMkP3k3zJk", "path": "/", "domain": ".douyu.com", "secure": false, "httpOnly": true, "expiry": 1616158558, "sameSite": "None"}, {"name": "wan_auth37wan", "value": "30190fc17c67VVDMYoUYNWjEcRp9IJC2ckkQq%2BI7lr%2FZe3jFG7GpNt4s3qf%2BAT2YBiLxU2kF9IGoriWPXwPlY1oO8YhpslzUE8y1CIshO7gZEkwJP8o", "path": "/", "domain": ".douyu.com", "secure": false, "httpOnly": true, "expiry": 1616158558, "sameSite": "None"}, {"name": "acf_uid", "value": "351608479", "path": "/", "domain": "www.douyu.com", "secure": false, "httpOnly": false, "expiry": 1616158558, "sameSite": "None"}, {"name": "acf_username", "value": "351608479", "path": "/", "domain": "www.douyu.com", "secure": false, "httpOnly": false, "expiry": 1616158558, "sameSite": "None"}, {"name": "acf_nickname", "value": "KiRaL1412", "path": "/", "domain": "www.douyu.com", "secure": false, "httpOnly": false, "expiry": 1616158558, "sameSite": "None"}, {"name": "acf_own_room", "value": "0", "path": "/", "domain": "www.douyu.com", "secure": false, "httpOnly": false, "expiry": 1616158558, "sameSite": "None"}, {"name": "acf_groupid", "value": "1", "path": "/", "domain": "www.douyu.com", "secure": false, "httpOnly": false, "expiry": 1616158558, "sameSite": "None"}, {"name": "acf_phonestatus", "value": "1", "path": "/", "domain": "www.douyu.com", "secure": false, "httpOnly": false, "expiry": 1616158558, "sameSite": "None"}, {"name": "acf_avatar", "value": "https%3A%2F%2Fapic.douyucdn.cn%2Fupload%2Favatar_v3%2F202003%2Fbea603ecfea449c1bcb7d86515825c5f_", "path": "/", "domain": "www.douyu.com", "secure": false, "httpOnly": false, "sameSite": "None"}, {"name": "acf_ct", "value": "0", "path": "/", "domain": "www.douyu.com", "secure": false, "httpOnly": false, "expiry": 1616158558, "sameSite": "None"}, {"name": "acf_ltkid", "value": "93792066", "path": "/", "domain": "www.douyu.com", "secure": false, "httpOnly": false, "expiry": 1616158558, "sameSite": "None"}, {"name": "acf_biz", "value": "1", "path": "/", "domain": "www.douyu.com", "secure": false, "httpOnly": false, "expiry": 1616158558, "sameSite": "None"}, {"name": "acf_stk", "value": "4fc8e75345880b09", "path": "/", "domain": "www.douyu.com", "secure": false, "httpOnly": false, "expiry": 1616158558, "sameSite": "None"}, {"name": "dy_did", "value": "bc436103e3ca33c70f13ecdb00061601", "path": "/", "domain": ".douyu.com", "secure": false, "httpOnly": false, "expiry": 1930989358, "sameSite": "None"}, {"name": "acf_did", "value": "bc436103e3ca33c70f13ecdb00061601", "path": "/", "domain": "www.douyu.com", "secure": false, "httpOnly": false, "expiry": 1930989358, "sameSite": "None"}, {"name": "Hm_lvt_e99aee90ec1b2106afe7ec3b199020a7", "value": "1615629345", "path": "/", "domain": ".douyu.com", "secure": false, "httpOnly": false, "expiry": 1647165359, "sameSite": "None"}, {"name": "Hm_lpvt_e99aee90ec1b2106afe7ec3b199020a7", "value": "1615629359", "path": "/", "domain": ".douyu.com", "secure": false, "httpOnly": false, "sameSite": "None"}]')
-
+    mycookies = os.environ["cookies"]
     listCookies = json.loads(mycookies)
 
     # 往browser里添加cookies
@@ -56,3 +56,7 @@ while(count<gift_num):
     count += 1
 
 browser.quit()
+
+
+
+
