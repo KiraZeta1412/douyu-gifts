1d1aacc1c52c95a1445a564480710aaa7571afb2
KiraZeta1412 <72505513+KiraZeta1412@users.noreply.github.com>
2021-03-27 22:10:54 +0800

    Update douyu_main.py

--- douyu_main.py
+++ douyu_main.py
@@ -43,8 +43,10 @@ if __name__ == "__main__":
 browser.execute_script('window.scrollTo(0,500)')
 browser.implicitly_wait(30)
 #点击背包
-if(browser.find_element_by_xpath('/html/body/div[7]/span').is_displayed()):
-    browser.find_element_by_xpath('/html/body/div[7]/span').click()
+if(browser.find_element_by_xpath('/html/body/div[7]/div[2]').is_displayed()):
+    browser.find_element_by_xpath('/html/body/div[7]/div[2]').click()
+    time.sleep(1)
+    browser.find_element_by_xpath('//*[@id="js-player-dialog"]/div[1]/div[9]/div[2]').click()
 bag = browser.find_element_by_xpath('//*[@id="js-player-toolbar"]/div[1]/div[2]/div/div[2]/div/div[4]/div/div/span').click()
 browser.implicitly_wait(30)
 while(not(browser.find_element_by_xpath('//*[@id="js-player-toolbar"]/div[1]/div[2]/div/div[2]/div/div[4]/div/div/div/div[3]/div/div[1]/ul[1]/li[1]').is_displayed())):
